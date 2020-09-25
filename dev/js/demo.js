a33f01f80b454563b4bde7bb419d711f91075840
hattran <63318539+hattran@users.noreply.github.com>
2020-09-25 15:37:28 -0400

    6 squares to circles animated

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -6,7 +6,16 @@ import {gsap} from "gsap";
 
 var timeOfAnimation = 4;
 
-gsap.to(".red-box",{duration: timeOfAnimation, alpha:0, rotate:360, x:500})
+gsap.from(".red-box",{duration: timeOfAnimation, alpha:0.5, rotate:360, x:600})
+gsap.from(".orange-box",{duration: timeOfAnimation, alpha:0.5, rotate:360, x:500})
+gsap.from(".yellow-box",{duration: timeOfAnimation, alpha:0.5, rotate:360, x:400})
+gsap.from(".green-box",{duration: timeOfAnimation, alpha:0.5, rotate:360, x:300})
+gsap.from(".blue-box",{duration: timeOfAnimation, alpha:0.5, rotate:360, x:200})
+gsap.from(".purple-box",{duration: timeOfAnimation, alpha:0.5, rotate:360, x:100})
 
-//gsap.to(".red-box",{duration: 3, borderRadius: "20"});
-//gsap.to(".red-box",{duration: timeOfAnimation, rotation:360, repeat:3});
+gsap.to(".red-box",{duration: 2, borderRadius: "50px", delay: timeOfAnimation})
+gsap.to(".orange-box",{duration: 2, borderRadius: "50px", delay: timeOfAnimation})
+gsap.to(".yellow-box",{duration: 2, borderRadius: "50px", delay: timeOfAnimation})
+gsap.to(".green-box",{duration: 2, borderRadius: "50px", delay: timeOfAnimation})
+gsap.to(".blue-box",{duration: 2, borderRadius: "50px", delay: timeOfAnimation})
+gsap.to(".purple-box",{duration: 2, borderRadius: "50px", delay: timeOfAnimation})
