fa1700fa82a4a12e9cce5919c7ca0615c259e493
hattran <63318539+hattran@users.noreply.github.com>
2020-10-05 09:50:04 -0400

    changed to web 2 site

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -4,18 +4,4 @@ import {gsap} from "gsap";
     Demo Script
 ----------------- */
 
-var timeOfAnimation = 4;
-
-gsap.from(".red-box",{duration: timeOfAnimation, alpha:0.5, rotate:360, x:600})
-gsap.from(".orange-box",{duration: timeOfAnimation, alpha:0.5, rotate:360, x:500})
-gsap.from(".yellow-box",{duration: timeOfAnimation, alpha:0.5, rotate:360, x:400})
-gsap.from(".green-box",{duration: timeOfAnimation, alpha:0.5, rotate:360, x:300})
-gsap.from(".blue-box",{duration: timeOfAnimation, alpha:0.5, rotate:360, x:200})
-gsap.from(".purple-box",{duration: timeOfAnimation, alpha:0.5, rotate:360, x:100})
-
-gsap.to(".red-box",{duration: 2, borderRadius: "50px", delay: timeOfAnimation})
-gsap.to(".orange-box",{duration: 2, borderRadius: "50px", delay: timeOfAnimation})
-gsap.to(".yellow-box",{duration: 2, borderRadius: "50px", delay: timeOfAnimation})
-gsap.to(".green-box",{duration: 2, borderRadius: "50px", delay: timeOfAnimation})
-gsap.to(".blue-box",{duration: 2, borderRadius: "50px", delay: timeOfAnimation})
-gsap.to(".purple-box",{duration: 2, borderRadius: "50px", delay: timeOfAnimation})
+console.log(gsap);
