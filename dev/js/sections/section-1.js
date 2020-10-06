ec58cc2ee42d6d26da30d5068d4990e2d8b1d380
hattran <63318539+hattran@users.noreply.github.com>
2020-10-06 15:28:04 -0400

    reanimated section 1

--- dev/js/sections/section-1.js
+++ dev/js/sections/section-1.js
@@ -1,6 +1,20 @@
 import {gsap} from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
 
+const section1TL = gsap.timeline();
+
+section1TL.to("#section-1 div",{duration: 2, alpha:0, x:800, stagger:0.25});
+
+        
 export function section1Animation(){
-    gsap.from("#section-1",{duration:1, alpha:0});
+    ScrollTrigger.create({
+        // markers: true,
+        animation: section1TL,
+        toggleActions: "play none none none",
+        trigger: "#section-1",
+        start:"top, 50%",
+        end: "bottom 50%"
+    });
 }
